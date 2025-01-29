4d0119787ac5e99057b9bd52adbd9d0b01261c6f
Marcos Pinedo <marcosps@hotmail.com>
2025-01-29 13:55:25 +0800

    Add files via upload

---
+++ RideHailingGraphApp/src/assets/images/index.ts
@@ -0,0 +1,11 @@
+export { default as UserBackground } from "./userbackground.png";
+export { default as p_map_1 } from "./Passenger/p_map_1.png";
+export { default as p_map_2 } from "./Passenger/p_map_2.png";
+export { default as d_map_2 } from "./Driver/d_map_2.png";
+export { default as d_map_3 } from "./Driver/d_map_3.png";
+export { default as d_map_4 } from "./Driver/d_map_4.png";
+export { default as d_map_5 } from "./Driver/d_map_5.png";
+export { default as no_ride_found } from "./no-ride-found.png";
+export { default as driverLocation } from "./driverLocation.png";
+export { default as userFeedback } from "./UserFeedback.png";
+export { default as UnderConstruction } from "./under-construction.png";
